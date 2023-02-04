43b034a5f7c6964e5c73ada61cb3623a575faa6b
A. B. Raihan <codebytes@As-iMac.local>
2023-02-04 22:37:06 +0600

    UPDATE: expand classNames features and make it like npm classNames package

--- src/utils/classNames.js
+++ src/utils/classNames.js
@@ -1,5 +1,16 @@
-export default function classNames(classes) {
-  return Object.keys(classes)
-    .filter((key) => classes[key])
-    .join(' ');
+/**
+ * dynamic classNames execute
+ * @param  {...any} classes - String | Object
+ * @returns {string} - string of classes
+ */
+export default function classNames(...classes) {
+  const classArray = classes.flat().map((classItem) => {
+    // only object types are allowed. here map from object keys
+    if (typeof classItem === 'object' && !Array.isArray(classItem) && classItem !== null) {
+      const keys = Object.keys(classItem).filter((key) => Boolean(classItem[key]));
+      return keys;
+    }
+    return classItem;
+  });
+  return classArray.flatMap((item) => item).filter(Boolean).join(' ');
 }
